4eecda028c23c95c54bf6930cc79564c39aa2bf4
Kanika Dewan <jugnumisal@Jugnus-MacBook-Pro.local>
2019-06-28 19:44:08 -0700

    removing interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,20 +1,27 @@
 import React , { Component }from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux';
+import { DH_NOT_SUITABLE_GENERATOR } from 'constants';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
         state = {
             error: null
         }
+
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
             this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.reqInterceptor);
         }
 
         errorConfirmedHandler = () => (
